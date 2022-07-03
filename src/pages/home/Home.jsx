3eaa514844f188836bd4350825e82abcb417b45f
OzanOcak <ozanocak@gmail.com>
2022-07-03 15:39:37 -0400

    dark mode

--- src/pages/home/Home.jsx
+++ src/pages/home/Home.jsx
@@ -1,10 +1,11 @@
+import "./home.scss";
 import Chart from "../../components/chart/Chart";
 import Featured from "../../components/featured/Featured";
 import Navbar from "../../components/navbar/Navbar";
 import Sidebar from "../../components/sidebar/Sidebar";
 import TableList from "../../components/table/Table";
 import Widget from "../../components/widget/Widget";
-import "./home.scss";
+
 function Home() {
   return (
     <div className="home">
