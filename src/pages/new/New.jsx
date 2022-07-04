908000288b14ef5fd20c605665eba52e027c6ef4
OzanOcak <ozanocak@gmail.com>
2022-07-04 14:42:23 -0400

    preparing data to save from new page / input handling

--- src/pages/new/New.jsx
+++ src/pages/new/New.jsx
@@ -14,6 +14,16 @@ import { db } from "../../firebase";
 
 const New = ({ inputs, title }) => {
   const [file, setFile] = useState("");
+  const [data, setData] = useState({});
+
+  const handleInput = (e) => {
+    const id = e.target.id;
+    const value = e.target.value;
+
+    setData({ ...data, [id]: value });
+  };
+
+  console.log(data);
 
   const handleAdd = async (e) => {
     e.preventDefault();
@@ -66,7 +76,12 @@ const New = ({ inputs, title }) => {
               {inputs.map((input) => (
                 <div className="formInput" key={input.id}>
                   <label>{input.label}</label>
-                  <input type={input.type} placeholder={input.placeholder} />
+                  <input
+                    id={input.id}
+                    type={input.type}
+                    placeholder={input.placeholder}
+                    onChange={handleInput}
+                  />
                 </div>
               ))}
               <button type="submit">Send</button>
